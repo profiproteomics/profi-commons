02150f98eac6b734a439dae316ee108aad416b2d
Laurent Martin <christophe.bruley@cea.fr>
2012-11-23 16:58:03 +0000

    Ajout un test unitaire bidon pour le plugin Cobertura de Jenkins...
    #6616

---
+++ profi_commons_scala/src/test/scala/fr/proline/util/AppTest.scala
@@ -0,0 +1,13 @@
+package fr.proline.util
+
+import org.junit.Test
+import com.weiglewilczek.slf4s.Logging
+
+class AppTest extends Logging {
+
+  @Test
+  def test() {
+    logger.debug("Just a test");
+  }
+
+}
