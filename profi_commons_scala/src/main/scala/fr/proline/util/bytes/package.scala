7b9cf2c384f5bc82258f771b67977ef2a2225041
David Bouyssié <David.Bouyssie@ipbs.fr>
2012-12-10 07:46:52 +0000

    Add new bytes related utils: - Array[Double] to Array[Byte] conversion - Array[Float] to Array[Byte] conversion - Array[Byte] to hexadecimal String conversion

---
+++ profi_commons_scala/src/main/scala/fr/proline/util/bytes/package.scala
@@ -0,0 +1,43 @@
+package fr.proline.util
+
+package object bytes {
+  
+  import java.nio.{ByteBuffer,ByteOrder,DoubleBuffer}
+  
+  def floatsToBytes( floats: Array[Float], littleEndian: Boolean = true ): Array[Byte] = {
+    
+    val endianess = if(littleEndian) ByteOrder.LITTLE_ENDIAN else ByteOrder.BIG_ENDIAN
+    
+    // Convert float to a byte buffer
+    val byteBuf = ByteBuffer.allocate(4 * floats.length).order(endianess)
+    floats.foreach { byteBuf.putFloat(_) }
+
+    // Convert byte buffer into a byte array
+    byteBuf.array()
+  }
+  
+  def doublesToBytes( doubles: Array[Double], littleEndian: Boolean = true ): Array[Byte] = {
+    
+    val endianess = if(littleEndian) ByteOrder.LITTLE_ENDIAN else ByteOrder.BIG_ENDIAN
+    
+    // Convert doubles to a byte buffer
+    val byteBuf = ByteBuffer.allocate(8 * doubles.length).order(endianess)
+    doubles.foreach { byteBuf.putDouble(_) }
+
+    // Convert byte buffer into a byte array
+    byteBuf.array()
+  }
+  
+  private val HEX_CHARS = "0123456789abcdef".toCharArray()
+
+  def bytes2Hex( bytes: Array[Byte] ): String = {
+    val chars = new Array[Char]( 2 * bytes.length )
+    for ( i <- 0 until bytes.length ) {
+      val b = bytes( i )
+      chars( 2 * i ) = HEX_CHARS( ( b & 0xF0 ) >>> 4 )
+      chars( 2 * i + 1 ) = HEX_CHARS( b & 0x0F )
+    }
+    new String( chars )
+  }
+  
+}
