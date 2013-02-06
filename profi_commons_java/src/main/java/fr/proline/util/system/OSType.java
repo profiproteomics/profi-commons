d1c5fd98d03b80ccbf6213468d6877ecdfc11942
Laurent Martin <christophe.bruley@cea.fr>
2013-02-06 10:38:07 +0000

    Add utilities to retrieve OS Type (Linux or Windows) and arch (32 or 64 bits). Used to load appropriate Mascot Parser native libraries.

---
+++ profi_commons_java/src/main/java/fr/proline/util/system/OSType.java
@@ -0,0 +1,13 @@
+package fr.proline.util.system;
+
+/**
+ * OS Type (Linux or Windows) and architecture (amd64 or x86/i386) of a plateform.
+ * 
+ * @author LMN
+ * 
+ */
+public enum OSType {
+
+    LINUX_I386, LINUX_AMD64, WINDOWS_X86, WINDOWS_AMD64
+
+}
---
+++ profi_commons_java/src/test/java/fr/proline/util/system/OSInfoTest.java
@@ -0,0 +1,22 @@
+package fr.proline.util.system;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OSInfoTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OSInfoTest.class);
+
+    @Test
+    public void testGetOSType() {
+	final OSType osType = OSInfo.getOSType();
+
+	assertNotNull("Returned OS Type", osType);
+
+	LOG.info("Detected OS Type : {}", osType);
+    }
+
+}
