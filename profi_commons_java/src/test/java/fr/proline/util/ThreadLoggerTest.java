9611d07dad938c8d4baac0778fd1d1005bcb7a2d
Laurent Martin <christophe.bruley@cea.fr>
2013-02-08 11:13:26 +0000

    Add an UncaughtExceptionHandler when loading Mascot parser native libraries.
    UncaughtExceptionHandler does not work in JUnit test cases because JUnit framework catches and handles thrown Exceptions.

---
+++ profi_commons_java/src/test/java/fr/proline/util/ThreadLoggerTest.java
@@ -0,0 +1,46 @@
+package fr.proline.util;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ThreadLoggerTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ThreadLoggerTest.class);
+
+    @Test
+    public void testThreadLoggerLogger() {
+	final Runnable target = new InnerTest();
+
+	final Thread thr = new Thread(target, "Thread-testThreadLogger");
+	thr.setPriority(Thread.NORM_PRIORITY);
+	thr.setUncaughtExceptionHandler(new ThreadLogger(LOG));
+
+	thr.start();
+
+	try {
+	    thr.join();
+
+	    LOG.debug("Thread teminated");
+	} catch (InterruptedException intEx) {
+	    LOG.warn("Thread.join() interrupted", intEx);
+	}
+
+    }
+
+    private class InnerTest implements Runnable {
+
+	public void run() {
+	    LOG.debug("Entering thread loop");
+
+	    for (int i = 0; i < 10; ++i) {
+		/* Will generate a divide by zero ArithmeticException */
+		final int value = 1 / (i - 5);
+		LOG.trace("Value: {}", value);
+	    }
+
+	}
+
+    }
+
+}
