0c84a48c360f51683a4c73b1428f85c8610a630c
Laurent Martin <christophe.bruley@cea.fr>
2013-03-22 10:38:32 +0000

    Don't use @sleep()@ to check interrupt state of current Thread. Re-throw @InterruptedException@ to allow interruption handling even if caller don't watch interrupt state.

--- profi_commons_scala/src/main/scala/fr/proline/api/service/IService.scala
+++ profi_commons_scala/src/main/scala/fr/proline/api/service/IService.scala
@@ -1,59 +1,76 @@
 package fr.proline.api.service
 
+import scala.collection.mutable.ArrayBuffer
+
 import com.weiglewilczek.slf4s.Logging
 
-trait IService extends Runnable with HasProgress {
-  
+trait IService extends Runnable with HasProgress with Logging {
+
   protected def beforeInterruption() = {}
-  
-  def runService() : Boolean
-  
+
+  def runService(): Boolean
+
   // Define a run method which implements the Thread interruption policy
   def run(): Unit = {
-    
-    // TODO LMN : better use Thread.interrupted() or Thread.currentThread().isInterrupted() to know if a thread is interrupted
-    this.registerOnProgressAction( Thread.sleep(1) )
-    
-    try {
-      this.runService()
-    } catch {
-      case ie: InterruptedException => {
-        this.beforeInterruption()
-        Thread.currentThread.interrupt() // very important to interrupt again
+
+    /* Check interrupt state before executing work */
+    if (Thread.interrupted()) {
+      logger.warn("Current thread interrupted before running Service")
+    } else {
+
+      registerOnProgressAction({
+
+        if (Thread.interrupted()) { // Check if current thread is interrupted
+          throw new InterruptedException("Current thread is interrupted")
+        }
+
+      })
+
+      try {
+        runService()
+      } catch {
+        case ie: InterruptedException => {
+          /* Log and re-throw InterruptedException */
+          logger.warn("Service IService.runService() interrupted", ie)
+
+          beforeInterruption()
+
+          throw ie
+        }
+
       }
-      case e => throw e
-      case _ => throw new Exception("uncatched exception")
-    }
-    
-  }  
-    
+
+    } // End if (current thread not interrupted)
+
+  }
+
 }
 
 trait HasProgress extends Logging {
-  
+
   case class Step
-  
+
   import scala.collection.mutable.ArrayBuffer
-  
+
   //val progressPlan: Array[Step]
-  
+
   private val progress = 0f
   private val onProgressActions = new ArrayBuffer[Function0[Unit]](0)
   //private var onStepActions = new Array[ArrayBuffer[Function0[Unit]]](progressPlan.length)
-  
+
   protected def >>>(): Unit = {
-    
+
     // Execute all registered actions
-    for( onProgressAction <- onProgressActions ) {
+    for (onProgressAction <- onProgressActions) {
       onProgressAction()
     }
-    
+
     // TODO: execute step actions
-    
+
   }
-  
-  def registerOnProgressAction( action: => Unit ): Unit = this.onProgressActions += { () => action }
-  
+
+  def registerOnProgressAction(action: => Unit): Unit = this.onProgressActions += { () => action }
+
   //def registerOnStepAction( stepNum: Int, action: () => Unit ): Unit = this.onStepActions(stepNum-1) += action
-  
+
 }
