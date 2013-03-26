0edf40d7bf64d8ba3b203d64b2154f61fdd35cfa
Laurent Martin <christophe.bruley@cea.fr>
2013-03-26 15:49:12 +0000

    Moves all methods to cast Scala @Any@ to primitive values into @primitives@ package object (ProFI-Commons-Scala).
    Update dependent modules in Proline-Core.
    
    refs #7383

--- profi_commons_scala/src/main/scala/fr/proline/util/primitives/package.scala
+++ profi_commons_scala/src/main/scala/fr/proline/util/primitives/package.scala
@@ -2,25 +2,49 @@ package fr.proline.util
 
 package object primitives {
 
-  object LongOrIntAsInt {
-    
-    def asInt(num: AnyVal): Int = num match {
-      case i: Int => i
-      case l: Long => l.toInt
-      case _ => throw new IllegalArgumentException("can't only take a Int or a Long as input")
+  /**
+   * Tries to cast any value to Scala Float primitive.
+   */
+  def toFloat(value: Any): Float = {
+
+    if (value.isInstanceOf[Float]) {
+      value.asInstanceOf[Float]
+    } else {
+      value.asInstanceOf[Number].floatValue
     }
-    implicit def anyVal2Int( num: AnyVal ): Int = asInt( num )
-    
+
   }
-  
-  object DoubleOrFloatAsFloat {
-    
-    def asFloat(num: AnyVal): Float = num match {
-      case f: Float => f
-      case d: Double => d.toFloat
-      case _ => throw new IllegalArgumentException("can't only take a Double or a Float as input")
+
+  /**
+   * Tries to cast any value to Scala Double primitive.
+   */
+  def toDouble(value: Any): Double = {
+
+    if (value.isInstanceOf[Double]) {
+      value.asInstanceOf[Double]
+    } else {
+      value.asInstanceOf[Number].doubleValue
     }
-    implicit def anyVal2Float( num: AnyVal ): Float = asFloat( num )
-    
+
   }
+
+  /**
+   * Tries to cast any value to Scala Int primitive (check if a long value can be casted into integer range).
+   */
+  def toInt(value: Any): Int = {
+
+    if (value.isInstanceOf[Int]) {
+      value.asInstanceOf[Int]
+    } else {
+      val longValue = value.asInstanceOf[Number].longValue
+
+      if ((longValue < Integer.MIN_VALUE) || ((longValue > Integer.MAX_VALUE))) {
+        throw new IllegalArgumentException("Integer value out of range");
+      }
+
+      longValue.asInstanceOf[Int];
+    }
+
+  }
+
 }
---
+++ profi_commons_scala/src/test/scala/fr/proline/util/PrimitivesTest.scala
@@ -0,0 +1,55 @@
+package fr.proline.util
+
+import org.junit.Assert._
+import org.junit.Test
+
+import fr.proline.util.primitives._
+
+import fr.proline.util.{ MathUtils => JavaMathUtils }
+
+@Test
+class PrimitivesTest {
+
+  val flValue: Float = 1.2345f
+
+  val dblValue: Double = 1.2345
+
+  @Test
+  def testFloat() {
+    assertEquals("Primitive Scala Float to Scala Float", flValue, toFloat(flValue), JavaMathUtils.EPSILON_FLOAT)
+    assertEquals("Primitive Scala Float to AnyVal to Scala Float", flValue, valToFloat(flValue), JavaMathUtils.EPSILON_FLOAT)
+
+    assertEquals("Primitive Scala Double to Scala Float", flValue, toFloat(dblValue), JavaMathUtils.EPSILON_FLOAT)
+    assertEquals("Primitive Scala Double to AnyVal to Scala Float", flValue, valToFloat(dblValue), JavaMathUtils.EPSILON_FLOAT)
+
+    assertEquals("Java Float Wrapper to Scala Float", flValue, toFloat(java.lang.Float.valueOf(flValue)), JavaMathUtils.EPSILON_FLOAT)
+    assertEquals("Java Float Wrapper to AnyVal to Scala Float", flValue, valToFloat(java.lang.Float.valueOf(flValue)), JavaMathUtils.EPSILON_FLOAT)
+
+    assertEquals("Java Double Wrapper to Scala Float", flValue, toFloat(java.lang.Double.valueOf(dblValue)), JavaMathUtils.EPSILON_FLOAT)
+    assertEquals("Java Double Wrapper to AnyVal to Scala Float", flValue, valToFloat(java.lang.Double.valueOf(dblValue)), JavaMathUtils.EPSILON_FLOAT)
+  }
+
+  @Test
+  def testDouble() {
+    assertEquals("Primitive Scala Float to Scala Double", dblValue, toDouble(flValue), JavaMathUtils.EPSILON_LOW_PRECISION)
+    assertEquals("Primitive Scala Float to AnyVal to Scala Double", dblValue, valToDouble(flValue), JavaMathUtils.EPSILON_LOW_PRECISION)
+
+    assertEquals("Primitive Scala Double to Scala Double", dblValue, toDouble(dblValue), JavaMathUtils.EPSILON_HIGH_PRECISION)
+    assertEquals("Primitive Scala Double to AnyVal to Scala Double", dblValue, valToDouble(dblValue), JavaMathUtils.EPSILON_HIGH_PRECISION)
+
+    assertEquals("Java Float Wrapper to Scala Double", dblValue, toDouble(java.lang.Float.valueOf(flValue)), JavaMathUtils.EPSILON_LOW_PRECISION)
+    assertEquals("Java Float Wrapper to AnyVal to Scala Double", dblValue, valToDouble(java.lang.Float.valueOf(flValue)), JavaMathUtils.EPSILON_LOW_PRECISION)
+
+    assertEquals("Java Double Wrapper to Scala Double", dblValue, toDouble(java.lang.Double.valueOf(dblValue)), JavaMathUtils.EPSILON_HIGH_PRECISION)
+    assertEquals("Java Double Wrapper to AnyVal to Scala Double", dblValue, valToDouble(java.lang.Double.valueOf(dblValue)), JavaMathUtils.EPSILON_HIGH_PRECISION)
+  }
+
+  def valToFloat(value: AnyVal): Float = {
+    toFloat(value)
+  }
+
+  def valToDouble(value: AnyVal): Double = {
+    toDouble(value)
+  }
+
+}
