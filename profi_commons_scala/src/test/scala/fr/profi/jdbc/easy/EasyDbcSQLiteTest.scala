0444a043d2e1f65daa63c8f69639e26c63bb8ddc
Laurent Martin <christophe.bruley@cea.fr>
2013-06-12 12:13:27 +0000

    Merge ORM_Type branch -> trunk.
    refs #7653

--- profi_commons_scala/src/test/scala/fr/profi/jdbc/easy/EasyDbcSQLiteTest.scala
+++ profi_commons_scala/src/test/scala/fr/profi/jdbc/easy/EasyDbcSQLiteTest.scala
@@ -13,20 +13,17 @@ class EasyDbcSQLiteTest {
     
     ezDBC.beginTransaction()
     
-    var generatedIntSum: Int = 0
     var generatedLongSum: Long = 0L
     ezDBC.executePrepared("INSERT INTO person VALUES (NULL, ?, ?)",true) { stmt =>
       for ( i <- 1 to 3 ) {   
-        stmt.executeWith( new java.util.Date, 1 )
-        generatedIntSum += stmt.generatedInt
+        stmt.executeWith( new java.util.Date, 1 )        
         generatedLongSum += stmt.generatedLong
       }
     }
     
     ezDBC.commitTransaction()
     
-    assertEquals( 3, ezDBC.selectInt("SELECT count(*) FROM person") )
-    assertEquals( 6, generatedIntSum )
+    assertEquals( 3, ezDBC.selectInt("SELECT count(*) FROM person") )   
     assertEquals( 6L, generatedLongSum )
     
     println( ezDBC.select("SELECT name FROM person") { r => r.nextStringOrElse("") } (0) )
