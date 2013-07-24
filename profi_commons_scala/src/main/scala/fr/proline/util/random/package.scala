2b116b0ce05186b692b1057d83be7761735601b3
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-07-24 11:28:25 +0000

    Add randomFloat and randomGaussian functions

--- profi_commons_scala/src/main/scala/fr/proline/util/random/package.scala
+++ profi_commons_scala/src/main/scala/fr/proline/util/random/package.scala
@@ -19,12 +19,19 @@ object random {
   def randomInt(minInclu: Int, maxInclu: Int): Int = {
     require(minInclu <= maxInclu)
     
-    if (minInclu == maxInclu)
-      minInclu
-    else
-      Random.nextInt(maxInclu + 1 - minInclu) + minInclu
+    if (minInclu == maxInclu) minInclu
+    else Random.nextInt(maxInclu + 1 - minInclu) + minInclu
   }
 
+  def randomFloat(minInclu: Float, maxExclu: Float): Float = {
+    require(minInclu <= maxExclu)
+    
+    if (minInclu == maxExclu)
+      minInclu
+    else
+      Random.nextFloat() * (maxExclu - minInclu) + minInclu
+  }
+  
   def randomDouble(minInclu: Double, maxExclu: Double): Double = {
     require(minInclu <= maxExclu)
     
@@ -32,6 +39,20 @@ object random {
       minInclu
     else
       Random.nextDouble() * (maxExclu - minInclu) + minInclu
+  }
+  
+  def randomGaussian(minInclu: Double, maxInclu: Double, stddev: Double): Double = {
+    require(minInclu <= maxInclu)
+    
+    if (minInclu == maxInclu)
+      minInclu
+    else {
+      val randomValue = Random.nextGaussian * stddev + (minInclu + maxInclu)/2
+      
+      if( randomValue < minInclu ) minInclu
+      else if ( randomValue > maxInclu ) maxInclu
+      else randomValue
+    }
   }
   
 }
