7b71b99b357d52779ed30739880bd269e287c51d
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-12-16 10:15:15 +0000

    Add IfNotNull and MapIfNotNull objects to enhance functional programming on nullable values

--- profi_commons_scala/src/main/scala/fr/proline/util/misc/package.scala
+++ profi_commons_scala/src/main/scala/fr/proline/util/misc/package.scala
@@ -4,6 +4,18 @@ import java.util.concurrent.atomic.AtomicLong
 
 /** Miscellaneous helpers */
 package object misc {
+  
+  object IfNotNull {
+    def apply[I,O]( i: I )( fn: => O ): Option[O] = {
+      if( i != null ) Some( fn ) else None
+    }
+  }
+  
+  object MapIfNotNull {
+    def apply[I,O]( i: I )( fn: I => O ): Option[O] = {
+      if( i != null ) Some( fn(i) ) else None
+    }
+  }
 
   trait InMemoryIdGen {
 
