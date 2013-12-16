a3084956ce8816b39894912abb19384db4f5f796
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-12-16 16:52:42 +0000

    Enable Jacks for JSON deserialization

--- profi_commons_scala/src/main/scala/fr/profi/util/serialization/package.scala
+++ profi_commons_scala/src/main/scala/fr/profi/util/serialization/package.scala
@@ -104,11 +104,11 @@ package object serialization {
       objectMapper.writeValue(writer, value)
     }
     
-    /*def deserialize[T: Manifest](value: String) : T = {
+    def deserialize[T: Manifest](value: String) : T = {
       JacksMapper.readValue[T](value)
-    }*/
+    }
   
-    
+    /*
     // Code snippet used for Jackson Scala Module deserialization
     
     def deserialize[T: Manifest](value: String) : T = {
@@ -126,7 +126,8 @@ package object serialization {
         def getActualTypeArguments = m.typeArguments.map(typeFromManifest).toArray
         def getOwnerType = null
       }
-    }
+    }*/
+    
   }
   
   private[this] object CustomDoubleSerializer {
