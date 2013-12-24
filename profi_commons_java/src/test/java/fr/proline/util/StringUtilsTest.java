7956f451fd4a692f98c8980d8037945cb908ff5d
David Bouyssié <David.Bouyssie@ipbs.fr>
2013-12-24 14:56:22 +0000

    Apply ProFI code formatter

--- profi_commons_java/src/test/java/fr/proline/util/StringUtilsTest.java
+++ profi_commons_java/src/test/java/fr/proline/util/StringUtilsTest.java
@@ -6,55 +6,55 @@ import org.junit.Test;
 
 public class StringUtilsTest {
 
-    @Test
-    public void testIsEmpty() {
-	String str = (String) null;
-	assertTrue("Chaine null", StringUtils.isEmpty(str));
+	@Test
+	public void testIsEmpty() {
+		String str = (String) null;
+		assertTrue("Chaine null", StringUtils.isEmpty(str));
 
-	str = "";
-	assertTrue("Chaine vide", StringUtils.isEmpty(str));
+		str = "";
+		assertTrue("Chaine vide", StringUtils.isEmpty(str));
 
-	str = " ";
-	assertTrue("Un espace", StringUtils.isEmpty(str));
+		str = " ";
+		assertTrue("Un espace", StringUtils.isEmpty(str));
 
-	str = "\t";
-	assertTrue("Une tabulation", StringUtils.isEmpty(str));
+		str = "\t";
+		assertTrue("Une tabulation", StringUtils.isEmpty(str));
 
-	str = "\t\t";
-	assertTrue("Deux tabulations", StringUtils.isEmpty(str));
+		str = "\t\t";
+		assertTrue("Deux tabulations", StringUtils.isEmpty(str));
 
-	str = "  ";
-	assertTrue("Deux espaces", StringUtils.isEmpty(str));
+		str = "  ";
+		assertTrue("Deux espaces", StringUtils.isEmpty(str));
 
-	str = " \t";
-	assertTrue("Espace + tab", StringUtils.isEmpty(str));
+		str = " \t";
+		assertTrue("Espace + tab", StringUtils.isEmpty(str));
 
-	str = "a";
-	assertFalse("Un caractère", StringUtils.isEmpty(str));
+		str = "a";
+		assertFalse("Un caractère", StringUtils.isEmpty(str));
 
-	str = "aa";
-	assertFalse("Deux caractères", StringUtils.isEmpty(str));
+		str = "aa";
+		assertFalse("Deux caractères", StringUtils.isEmpty(str));
 
-	str = " a\t";
-	assertFalse("Espace, un caractère, une tabulation", StringUtils.isEmpty(str));
+		str = " a\t";
+		assertFalse("Espace, un caractère, une tabulation", StringUtils.isEmpty(str));
 
-	str = "  toto\t";
-	assertFalse("Deux espaces, chaine non vide, une tabulation", StringUtils.isEmpty(str));
-    }
+		str = "  toto\t";
+		assertFalse("Deux espaces, chaine non vide, une tabulation", StringUtils.isEmpty(str));
+	}
 
-    @Test
-    public void testResidue() {
-	final char expectedChar = 'A';
+	@Test
+	public void testResidue() {
+		final char expectedChar = 'A';
 
-	final String expectedStr = "A";
+		final String expectedStr = "A";
 
-	final String strResidue = StringUtils.convertCharResidueToString(expectedChar);
+		final String strResidue = StringUtils.convertCharResidueToString(expectedChar);
 
-	assertEquals("Char -> String", expectedStr, strResidue);
+		assertEquals("Char -> String", expectedStr, strResidue);
 
-	final char charResidue = StringUtils.convertStringResidueToChar(strResidue);
+		final char charResidue = StringUtils.convertStringResidueToChar(strResidue);
 
-	assertEquals("String -> char", expectedChar, charResidue);
-    }
+		assertEquals("String -> char", expectedChar, charResidue);
+	}
 
 }
