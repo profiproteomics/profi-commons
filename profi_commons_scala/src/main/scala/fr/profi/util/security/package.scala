c779d6a6a758e5cc8cab3883cf6d0699172826e1
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-04-28 12:14:58 +0000

    Add sha256Hex function for message/password hashing

--- profi_commons_scala/src/main/scala/fr/profi/util/security/package.scala
+++ profi_commons_scala/src/main/scala/fr/profi/util/security/package.scala
@@ -5,9 +5,15 @@ import fr.proline.util.bytes._
 
 package object security {
   
-  def md5Hex( password: String ): String = {
+  def md5Hex( text: String ): String = {
     val md = MessageDigest.getInstance("MD5")
-    md.update(password.getBytes())
+    md.update(text.getBytes("UTF-8"))
+    bytes2HexString(md.digest())
+  }
+  
+  def sha256Hex( text: String ): String = {
+    val md = MessageDigest.getInstance("SHA-256")
+    md.update(text.getBytes("UTF-8"))
     bytes2HexString(md.digest())
   }
 
