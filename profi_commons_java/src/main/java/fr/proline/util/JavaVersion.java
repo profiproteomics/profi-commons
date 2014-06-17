e8755e1ca7c2267a22eb1beaa8036a78c9b3fee8
Veronique Dupierris <veronique.dupierris@cea.fr>
2014-06-17 06:59:59 +0000

    Passage en snapshot

---
+++ profi_commons_java/src/main/java/fr/proline/util/JavaVersion.java
@@ -0,0 +1,15 @@
+package fr.proline.util;
+
+import fr.proline.util.version.IVersion;
+
+public class JavaVersion implements IVersion {
+
+    public String getModuleName() {
+	return "ProFI-Commons-Java";
+    }
+
+    public String getVersion() {
+	return "0.0.6-SNAPSHOT_20140515-0812_VD225637";
+    }
+
+}
