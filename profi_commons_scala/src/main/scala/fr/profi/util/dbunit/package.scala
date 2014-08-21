d2b5cfb7e833255603b1c7b736ed3ca8849cca83
David Bouyssié <David.Bouyssie@ipbs.fr>
2014-08-21 08:49:39 +0000

    Add new DbUnit util in order to parse XML datasets using the scala XML API

---
+++ profi_commons_scala/src/main/scala/fr/profi/util/dbunit/package.scala
@@ -0,0 +1,63 @@
+package fr.profi.util
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
+import fr.profi.util.resources.pathToFileOrResourceToFile
+
+/**
+ * @author David Bouyssie
+ *
+ */
+package object dbunit {
+  
+  def parseDbUnitDataset( datasetPath: String ): Map[String,ArrayBuffer[Map[String,String]]] = {
+    
+    // Workaround for issue "Non-namespace-aware mode not implemented"
+    // We use the javax SAXParserFactory with a custom configuration
+    // Source:  http://stackoverflow.com/questions/11315439/ignore-dtd-specification-in-scala
+    val saxParserFactory = javax.xml.parsers.SAXParserFactory.newInstance()
+    // TODO: check if it also works without settting validating to false
+    saxParserFactory.setValidating(false)
+
+    // Instantiate the XML loader using the javax SAXParser
+    val xmlLoader = xml.XML.withSAXParser(saxParserFactory.newSAXParser)
+    
+    parseDbUnitDataset( datasetPath, xmlLoader )
+  }
+  
+  def parseDbUnitDataset( datasetPath: String, xmlLoader: xml.factory.XMLLoader[xml.Elem] ): Map[String,ArrayBuffer[Map[String,String]]] = {
+
+    // Load the dataset
+    val xmlDoc = xmlLoader.loadFile( pathToFileOrResourceToFile(datasetPath,this.getClass) )
+    
+    val recordsByTableName = new HashMap[String,ArrayBuffer[Map[String,String]]]
+    
+    // Iterate over dataset nodes
+    for( xmlNode <- xmlDoc.child ) {
+      
+      val attrs = xmlNode.attributes
+      
+      // Check node has defined attributes
+      if( attrs.isEmpty == false ) {
+        
+        val tableName = xmlNode.label
+        
+        val recordBuilder = Map.newBuilder[String,String]
+        
+        // Iterate over node attributes
+        for( attr <- attrs ) {
+          val str = attr.value.text
+          recordBuilder += attr.key -> attr.value.text
+        }
+        
+        // Append record to the records of this table
+        val records = recordsByTableName.getOrElseUpdate(tableName, new ArrayBuffer[Map[String,String]]() )
+        records += recordBuilder.result()
+        
+      }
+    }
+    
+    recordsByTableName.toMap
+  }
+
+}
