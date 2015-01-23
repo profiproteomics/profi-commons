6e528bd00f796010337fbad8d65228348610790e
Julie Poisat <julie.poisat@ipbs.fr>
2015-01-23 13:02:17 +0000

    Add DefaultMascotSearchForm Rename field 'userId' into 'ownerMongoId'

--- profi_pom/src/main/scala/fr/profi/msangel/om/MsiSearchForm.scala
+++ profi_pom/src/main/scala/fr/profi/msangel/om/MsiSearchForm.scala
@@ -2,11 +2,41 @@ package fr.profi.msangel.om
 
 import play.api.libs.json.JsObject
 import org.joda.time.DateTime
+import play.api.libs.json.Json
 
 /**
  * Model for Mass Spectrometry Identification search form
  * (parameters requiered by search server).
  */
+//case class MsiSearchForm(
+//
+//  /** Parameters */
+//  val targetURL: String, //TODO: move to MsiTask? (replace by isFake)
+////  val searchEngine: SearchEngine.Value,
+//  val paramMap: JsObject,
+//  val isTemplate: Boolean = false,
+//
+//  /** For templates only */
+//  val name: Option[String] = None,
+//  val ownerMongoId: Option[String] = None, 
+//  val registTime: Option[DateTime] = None
+//  
+//) {
+//  //extends IMsiObject {
+//
+//  /** Requirements */
+//  require(targetURL != null, "targetURL must not be null.")
+//  require(targetURL.isEmpty() == false, "targetURL must not be empty.")
+////  require(searchEngine != null, "Search engine must not be null")
+//  require(paramMap != null, "Some paramMap must be provided")
+//
+//  /** Some class utilities */
+//  def get(key: MascotSearchParam.Value): String = ((paramMap \ key.toString()).asOpt[String]).getOrElse("")
+//  def getOpt(key: MascotSearchParam.Value): Option[String] = (paramMap \ key.toString()).asOpt[String]
+//  
+//}
+
+// BEFORE REFACTORING
 
 case class MsiSearchForm(
 
@@ -18,9 +48,9 @@ case class MsiSearchForm(
 
   /** For templates only */
   val name: Option[String] = None,
-  //  val userId: Option[Int] = None,
-  val userId: Option[String] = None, //MongoId
-  val registTime: Option[DateTime] = None) {
+  val ownerMongoId: Option[String] = None,
+  val registTime: Option[DateTime] = None
+  ) {
   //extends IMsiObject {
 
   /** Requirements */
@@ -32,14 +62,15 @@ case class MsiSearchForm(
   /** Some class utilities */
   def get(key: MascotSearchParam.Value): String = ((paramMap \ key.toString()).asOpt[String]).getOrElse("")
   def getOpt(key: MascotSearchParam.Value): Option[String] = (paramMap \ key.toString()).asOpt[String]
+}
 
-  //  //TODO: in dedicated tab (runTask())
-  //  protected val requiredFields = Seq(
-  //    //TODO: required fields
-  //    MascotSearchParam.FILE
-  //  )
-
-  //  protected def checkForm(): Boolean = {
-  //  }
-
+object DefaultMascotSearchForm {
+  def apply() = {
+    MsiSearchForm(
+      targetURL = "http://www.matrixscience.com/cgi",
+      searchEngine = SearchEngine.MASCOT,
+      paramMap = Json.obj(),
+      isTemplate = true
+    )
+  }
 }
