e248ce721b72f21d39aeceead710023514c2613d
Julie Poisat <julie.poisat@ipbs.fr>
2015-02-20 17:02:21 +0000

    FollowUpEvent classes extend trait IFollowUpEvent

--- profi_pom/src/main/scala/fr/profi/msangel/om/workflow/operation/FollowUpEvents.scala
+++ profi_pom/src/main/scala/fr/profi/msangel/om/workflow/operation/FollowUpEvents.scala
@@ -1,14 +1,16 @@
 package fr.profi.msangel.om.workflow.operation
 
+trait IFollowUpEvent //won't mess up serialization?
+
 /** Send a notification by e-mail */
-case class EMailNotification(emailAdress: String) {
+case class EMailNotification(emailAdress: String) extends IFollowUpEvent {
   def execute(): String = null
   //  def getAsStrings() = ("Email notification at", emailAdress)
   override def toString() = emailAdress
 }
 
 /** Execute a command line */
-case class CmdLineExecution(cmdLine: String) {
+case class CmdLineExecution(cmdLine: String) extends IFollowUpEvent {
   def execute(): String = null
   override def toString() = cmdLine
 }
@@ -18,7 +20,7 @@ case class WebServiceCall(
   url: String,
   httpMethod: String,
   body: Option[String]
-) {
+) extends IFollowUpEvent {
   def execute(): String = null
   override def toString() = url
 }
