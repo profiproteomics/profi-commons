26b7402b64544c3ffd144e070e8d51c4a0b950bf
Julie Poisat <julie.poisat@ipbs.fr>
2015-04-01 09:26:28 +0000

    MSAngelServerConfig extends IMongoDbEtity; Add some requirements

--- profi_pom/src/main/scala/fr/profi/msangel/om/MSAngelServerConfig.scala
+++ profi_pom/src/main/scala/fr/profi/msangel/om/MSAngelServerConfig.scala
@@ -1,8 +1,24 @@
 package fr.profi.msangel.om
 
+import fr.profi.pwx.util.mongodb.IMongoDbEntity
+import reactivemongo.bson.BSONObjectID
+import fr.profi.msangel.om.workflow.operation.conversion.MsConvert
+
 case class MSAngelServerConfig(
-  var msconvertPath: String = "", //C:\Program Files\ProteoWizard 3.0.7076\msconvert.exe
+
+      var id: Option[BSONObjectID] = None,
+  var msconvertPath: String = "",
   var extractmsnPath: String = "",
   var raw2mzdbPath: String = "",
   var mzdbaccessPath: String = ""
-)
+    
+) extends IMongoDbEntity {
+
+    require(msconvertPath != null, "msconvertPath must not be null")
+
+    require(extractmsnPath != null, "extractmsnPath must not be null")
+
+    require(raw2mzdbPath != null, "raw2mzdbPath must not be null")
+
+    require(mzdbaccessPath != null, "mzdbaccessPath must not be null")
+}
