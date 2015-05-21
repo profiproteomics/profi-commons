08382b11bf592d17dab542225fc50a62b9b46af6
Julie Poisat <julie.poisat@ipbs.fr>
2015-05-21 14:06:33 +0000

    Add utility "getParamsAsString"

--- profi_pom/src/main/scala/fr/profi/msangel/om/msi/MsiSearchForm.scala
+++ profi_pom/src/main/scala/fr/profi/msangel/om/msi/MsiSearchForm.scala
@@ -40,6 +40,57 @@ case class MsiSearchForm(
   /** Some class utilities */
   def get(key: MascotSearchParam.Value): String = ((paramMap \ key.toString()).asOpt[String]).getOrElse("")
   def getOpt(key: MascotSearchParam.Value): Option[String] = (paramMap \ key.toString()).asOpt[String]
+  
+  /** Get text to display easily parameters **/
+  def getParamsAsString(): String = {
+
+    val MSP = MascotSearchParam
+
+    /* Utility */
+    def _param2string(param: MSP.Value): String = {
+      val str = ((this.paramMap \ param.toString()).asOpt[String]).getOrElse("")
+      if (param == MascotSearchParam.DECOY || param == MascotSearchParam.ERRORTOLERANT) {
+        if (str == "1") "true" else "false"
+      } else str
+    }
+
+    val strBld = new StringBuilder()
+    
+    /* Parameters common to all searches */
+    strBld ++= s"""
+Database(s) : ${_param2string(MSP.DB)}
+Taxonomy : ${_param2string(MSP.TAXONOMY).dropWhile { c => c.toString() == "." || c.toString() == " " }}
+Cleavage enzyme: ${_param2string(MSP.CLE)}
+Fixed modifications : ${_param2string(MSP.MODS)}
+Variable modifications : ${_param2string(MSP.IT_MODS)}
+Protein mass : ${_param2string(MSP.SEG)} (kDa)
+Max. missed cleavages : ${_param2string(MSP.PFA)}
+Peptide mass tolerance : ${_param2string(MSP.TOL)} ${_param2string(MSP.TOLU)}
+Peptide charge : ${_param2string(MSP.CHARGE)}
+Precursor m/z : ${_param2string(MSP.PRECURSOR)}
+Misassigned 13C : ${_param2string(MSP.PEP_ISOTOPE_ERROR)}
+Report : ${_param2string(MSP.REPORT)} top hits
+Decoy : ${_param2string(MSP.DECOY)}
+Mass : ${_param2string(MSP.MASS)}
+"""
+    //TODO: add project
+
+    /* Add MS/MS parameters if applicable */
+    if (_param2string(MSP.SEARCH) == "MIS") strBld ++= s"""
+MS/MS IONS SEARCH : true
+Error tolerant : ${_param2string(MSP.ERRORTOLERANT)}
+MS/MS tolerance : ${_param2string(MSP.ITOL)} ${_param2string(MSP.ITOLU)}
+Data format : ${_param2string(MSP.FORMAT)}
+Quantitation : ${_param2string(MSP.QUANTITATION)}
+Instrument : ${_param2string(MSP.INSTRUMENT)}
+"""
+    /* Otherwise say it's not an MS/MS search */
+    else strBld ++= """
+MS/MS IONS SEARCH : false
+"""
+
+    strBld.result()
+  }
 }
 
 object DefaultMascotSearchForm {
