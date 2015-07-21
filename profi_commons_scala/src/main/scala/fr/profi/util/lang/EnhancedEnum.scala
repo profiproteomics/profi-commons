88e8d368d7666f688529d6eaf491f04b8851e4ba
David Bouyssié <David.Bouyssie@ipbs.fr>
2015-07-21 14:51:12 +0000

    Add EnhancedEnum trait (extended version of usual Scala Enumeration)

---
+++ profi_commons_scala/src/main/scala/fr/profi/util/lang/EnhancedEnum.scala
@@ -0,0 +1,8 @@
+package fr.profi.util.lang
+
+trait EnhancedEnum extends Enumeration {
+  lazy val valueByKey = values.map(v => (v.toString, v)).toMap
+  def maybeNamed(s: String): Option[this.type#Value] = valueByKey.get(s)
+  
+  implicit def enumToString(value: this.type#Value): String = value.toString
+}
