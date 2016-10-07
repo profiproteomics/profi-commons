736322ae1a5942081f756e1ad92442f66af50124
David Bouyssié <David.Bouyssie@ipbs.fr>
2016-10-07 08:25:07 +0000

    Add nearlyEquals util

--- profi_commons_java/src/main/java/fr/profi/util/MathUtils.java
+++ profi_commons_java/src/main/java/fr/profi/util/MathUtils.java
@@ -16,6 +16,11 @@ public final class MathUtils {
 	 * For <code>double</code> with <em>double precision</em> computations.
 	 */
 	public static final double EPSILON_HIGH_PRECISION = 1e-14;
+	
+	public static boolean nearlyEquals(double a, double b, double eps) {
+		if (a==b) return true;
+		return Math.abs(a - b) < eps;
+	}
 
 	private MathUtils() {
 	}
