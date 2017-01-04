440a47c31cbd395ea47e1ea21255654930a0d182
David Bouyssié <David.Bouyssie@ipbs.fr>
2017-01-04 14:35:22 +0000

    [ProFI-Commons-Scala] Synchronize Metric methods to be thread safe + use string interpolation in the toString method

--- profi_commons_scala/src/main/scala/fr/profi/util/metrics/Metric.scala
+++ profi_commons_scala/src/main/scala/fr/profi/util/metrics/Metric.scala
@@ -5,62 +5,75 @@ import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 
 class Metric(val name: String) {
 
-  val counters = collection.mutable.Map[String, Integer]()
-  val summaries = collection.mutable.Map[String, SummaryStatistics]()
-  val statistics = collection.mutable.Map[String, DescriptiveStatistics]()
+  private val counters = collection.mutable.Map[String, Integer]()
+  private val counters_lock = new Object()
   
-  def setCounter(name: String, value: Integer) = {
+  private val summaries = collection.mutable.Map[String, SummaryStatistics]()
+  private val summaries_lock = new Object()
+  
+  private val statistics = collection.mutable.Map[String, DescriptiveStatistics]()
+  private val statistics_lock = new Object()
+  
+  def getCounter(name: String) = counters_lock.synchronized {
+    counters.getOrElseUpdate(name, 0)
+  }
+  
+  def setCounter(name: String, value: Integer) = counters_lock.synchronized {
    counters(name) =  value   
   }
   
-  def getCounter(name: String) = {
-   counters.getOrElseUpdate(name, 0)   
-  }
-  
-  def incr(name: String) = {
+  def incr(name: String) = counters_lock.synchronized {
    counters(name) = counters.getOrElseUpdate(name, 0) + 1
   }
 
-  def decr(name: String) = {
+  def decr(name: String) = counters_lock.synchronized {
    counters(name) = counters.getOrElseUpdate(name, 0) - 1
   }
 
-  def addValue(name: String, d: Double) = {
+  def addValue(name: String, d: Double) = summaries_lock.synchronized {
 	 summaries.getOrElseUpdate(name, new SummaryStatistics).addValue(d)
   }
   
-  def storeValue(name: String, d: Double) = {
+  // DBO: The API is ambiguous => it is not straightforward to understand the difference between addValue and storeValue
+  // Summary is a subset of descriptive stats => why not compute DescriptiveStatistics systematically ?
+  def storeValue(name: String, d: Double) = statistics_lock.synchronized {
 	 statistics.getOrElseUpdate(name, new DescriptiveStatistics).addValue(d)
   }
-    
+
   override def toString() = {
+
     val strBuilder = StringBuilder.newBuilder
-    strBuilder.append(name).append("\n")
-    strBuilder.append("## Counters ##").append("\n")    
-    for (c <- counters) {
-      strBuilder.append(c._1).append(" = ").append(c._2).append('\n')
+    strBuilder ++= s"$name\n"
+    strBuilder ++= "## Counters ##\n"
+
+    for ((k,v) <- counters) {
+      strBuilder ++= s"$k = $v\n"
     }
-    strBuilder.append("## Statistical Summaries ##").append("\n")    
-    for (c <- summaries) {
-      strBuilder.append(c._1).append(".N = ").append(c._2.getN()).append('\n')
-      strBuilder.append(c._1).append(".min = ").append(c._2.getMin()).append('\n')
-      strBuilder.append(c._1).append(".max = ").append(c._2.getMax()).append('\n')
-      strBuilder.append(c._1).append(".mean = ").append(c._2.getMean()).append('\n')
-      strBuilder.append(c._1).append(".geomMean = ").append(c._2.getGeometricMean()).append('\n')
-      strBuilder.append(c._1).append(".standard_deviation = ").append(c._2.getStandardDeviation()).append('\n')
+
+    strBuilder ++= "## Statistical Summaries ##\n"
+
+    for ((k,v) <- summaries) {
+      strBuilder ++= s"$k.N = ${v.getN}\n"
+      strBuilder ++= s"$k.min = ${v.getMin}\n"
+      strBuilder ++= s"$k.max = ${v.getMax}\n"
+      strBuilder ++= s"$k.mean = ${v.getMean}\n"
+      strBuilder ++= s"$k.geom_mean = ${v.getGeometricMean}\n"
+      strBuilder ++= s"$k.standard_deviation = ${v.getStandardDeviation}\n"
     }
-     for (c <- statistics) {
-      strBuilder.append(c._1).append(".N = ").append(c._2.getN()).append('\n')
-      strBuilder.append(c._1).append(".min = ").append(c._2.getMin()).append('\n')
-      strBuilder.append(c._1).append(".max = ").append(c._2.getMax()).append('\n')
-      strBuilder.append(c._1).append(".mean = ").append(c._2.getMean()).append('\n')
-      strBuilder.append(c._1).append(".25 = ").append(c._2.getPercentile(25)).append('\n')      
-      strBuilder.append(c._1).append(".median = ").append(c._2.getPercentile(50)).append('\n')
-      strBuilder.append(c._1).append(".75 = ").append(c._2.getPercentile(75)).append('\n')
-      strBuilder.append(c._1).append(".skewness = ").append(c._2.getSkewness()).append('\n')
-      strBuilder.append(c._1).append(".kurtosis = ").append(c._2.getKurtosis()).append('\n')
-      strBuilder.append(c._1).append(".standard_deviation = ").append(c._2.getStandardDeviation()).append('\n')
-     }
+
+    for ((k,v) <- statistics) {
+      strBuilder ++= s"$k.N = ${v.getN}\n"
+      strBuilder ++= s"$k.min = ${v.getMin}\n"
+      strBuilder ++= s"$k.max = ${v.getMax}\n"
+      strBuilder ++= s"$k.mean = ${v.getMean}\n"
+      strBuilder ++= s"$k.25 = ${v.getPercentile(25)}\n"
+      strBuilder ++= s"$k.median = ${v.getPercentile(50)}\n"
+      strBuilder ++= s"$k.75 = ${v.getPercentile(75)}\n"
+      strBuilder ++= s"$k.skewness = ${v.getSkewness}\n"
+      strBuilder ++= s"$k.kurtosis = ${v.getKurtosis}\n"
+      strBuilder ++= s"$k.standard_deviation = ${v.getStandardDeviation}\n"
+    }
+
     strBuilder.toString
   }
 }
