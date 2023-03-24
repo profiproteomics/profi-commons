a505829ddac8a52467f466f8c8b2bcb0f38304dc
VeroDup <veronique.dupierris@cea.fr>
2023-03-24 11:37:33 +0100

    Add method to solve/transpose matrix

--- profi_commons_java/src/main/java/fr/profi/util/MathUtils.java
+++ profi_commons_java/src/main/java/fr/profi/util/MathUtils.java
@@ -1,5 +1,8 @@
 package fr.profi.util;
 
+import org.apache.commons.math3.linear.*;
+
+
 public final class MathUtils {
 
 	/**
@@ -25,4 +28,32 @@ public final class MathUtils {
 	private MathUtils() {
 	}
 
+	public static double[][] matrixSolver(double[][] coefficients, double[][] values, boolean coeffTransposed ){
+		if(values == null || values.length ==0)
+			return new double[0][0];
+
+		double[][] tCoefficients = coeffTransposed ? coefficients : transposeMatrix(coefficients);
+		double[][] newValues = new double[values.length][values[0].length];
+		RealMatrix coeffMatrix = new Array2DRowRealMatrix(tCoefficients,false );
+		DecompositionSolver solver = new LUDecomposition(coeffMatrix).getSolver();
+		RealVector constants = null;
+		RealVector result = null;
+		for (int i =0; i<values.length; i++){
+			double[] rowVal = values[i];
+			constants = new ArrayRealVector(rowVal, false);
+			result = solver.solve(constants);
+			newValues[i] = result.toArray();
+		}
+		return newValues;
+	}
+
+	public static double[][] transposeMatrix(double [][] m){
+		int d1 = m.length;
+		int d2 = m[0].length;
+		double[][] temp = new double[d2][d1];
+		for (int i = 0; i < d1; i++)
+			for (int j = 0; j < d2; j++)
+				temp[j][i] = m[i][j];
+		return temp;
+	}
 }
